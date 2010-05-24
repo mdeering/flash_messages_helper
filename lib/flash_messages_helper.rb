43535de81b50bc0fcebd4d7e1e6aed7e91453e04
Michael Deering <mdeering@mdeering.com>
2010-05-23 22:52:57 -0600

    Have the new configuration working and placeholders for deprecation warnings.

--- lib/flash_messages_helper.rb
+++ lib/flash_messages_helper.rb
@@ -1,22 +1,62 @@
 module FlashMessagesHelper
 
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+     attr_accessor :css_class, :dom_id, :wrapper
+     def initialize
+       @css_class = lambda { |key| "#{key}" }
+       @dom_id    = lambda { |key| "flash-#{key}" }
+       @wrapper   = :div
+     end
+   end
+
   def self.included(target)
-    target.cattr_accessor :flash_message_class_proc
-    target.flash_message_class_proc = lambda { |key| "#{key}" }
-    target.cattr_accessor :flash_message_id_proc
-    target.flash_message_id_proc = lambda { |key| "flash-#{key}" }
-    target.cattr_accessor :flash_message_tag
-    target.flash_message_tag     = :div
+    # target.cattr_accessor :flash_message_class_proc
+    # target.flash_message_class_proc = lambda { |key| "#{key}" }
+    # target.cattr_accessor :flash_message_id_proc
+    # target.flash_message_id_proc = lambda { |key| "flash-#{key}" }
+    # target.cattr_accessor :flash_message_tag
+    # target.flash_message_tag     = :div
+    target.extend ClassMethods
     target.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def flash_message_class_proc=(value)
+      # TODO: Deprication Warning!
+      FlashMessagesHelper.configure do |c|
+        c.css_class = value
+      end
+    end
+    def flash_message_id_proc=(value)
+      # TODO: Deprication Warning!
+      FlashMessagesHelper.configure do |c|
+        c.dom_id = value
+      end
+    end
+    def flash_message_tag=(value)
+      # TODO: Deprication Warning!
+      FlashMessagesHelper.configure do |c|
+        c.wrapper = value
+      end
+    end
   end
 
   module InstanceMethods
     def flash_messages(options = {})
       ret = []
       flash.each do |key, value|
-        ret << content_tag(flash_message_tag, value, {
-            :class => flash_message_class_proc.call(key),
-            :id    => flash_message_id_proc.call(key)
+        ret << content_tag(FlashMessagesHelper.configuration.wrapper, value, {
+            :class => FlashMessagesHelper.configuration.css_class.call(key),
+            :id    => FlashMessagesHelper.configuration.dom_id.call(key)
           }.merge(options)
         )
       end
@@ -26,4 +66,4 @@ module FlashMessagesHelper
 
 end
 
-ActionView::Base.send(:include, FlashMessagesHelper)
+ActionView::Base.send(:include, FlashMessagesHelper) if defined?(ActionView::Base)
--- spec/flash_messages_helper_spec.rb
+++ spec/flash_messages_helper_spec.rb
@@ -6,18 +6,21 @@ ActionView::Base.send(:include, FlashMessagesHelper)
 
 describe FlashMessagesHelper do
 
-  view = ActionView::Base.new
-  controller = ActionController::Base.new
-  view.controller = controller
+  before do
+    @view = ActionView::Base.new
+    @controller = ActionController::Base.new
+    @view.controller = @controller
+    FlashMessagesHelper.configuration = FlashMessagesHelper::Configuration.new
+  end
 
   it 'will have nothing to display of the flash is not set in any way' do
-    controller.stub!(:flash).and_return({})
-    view.flash_messages.should == ''
+    @controller.stub!(:flash).and_return({})
+    @view.flash_messages.should == ''
   end
 
   it 'will return a div with the error message' do
-    controller.stub!(:flash).and_return({:error => 'There was an error'})
-    view.flash_messages.should == "<div class=\"error\" id=\"flash-error\">There was an error</div>"
+    @controller.stub!(:flash).and_return({:error => 'There was an error'})
+    @view.flash_messages.should == "<div class=\"error\" id=\"flash-error\">There was an error</div>"
   end
 
   {
@@ -25,23 +28,25 @@ describe FlashMessagesHelper do
     :flash_message_id_proc    => lambda { |key| "flash-#{key}-message"},
     :flash_message_tag        => :p
   }.each do |singleton_variable, value|
-    it "should create flash_messages class (singleton) variable #{singleton_variable} on its included class" do
-      ActionView::Base.send(singleton_variable).should_not == nil
+    it "should give deprication warnings for 0.1.0 usage but still set configuration points" do
       ActionView::Base.send("#{singleton_variable}=", value)
-      ActionView::Base.send(singleton_variable).should == value
+      FlashMessagesHelper.configuration.css_class == value if singleton_variable == :flash_message_class_proc
+      FlashMessagesHelper.configuration.dom_id    == value if singleton_variable == :flash_message_id_proc
+      FlashMessagesHelper.configuration.wrapper   == value if singleton_variable == :flash_message_tag
     end
   end
 
   # Now that the defaults have been set
 
   it 'will return a p with the error message and the defaults set differently' do
-    controller.stub!(:flash).and_return({:error => 'There was an error'})
-    view.flash_messages.should == "<p class=\"error-message hiddable\" id=\"flash-error-message\">There was an error</p>"
+    FlashMessagesHelper.configuration.wrapper = :p
+    @controller.stub!(:flash).and_return({:error => 'There was an error'})
+    @view.flash_messages.should == "<p class=\"error\" id=\"flash-error\">There was an error</p>"
   end
 
   it 'will still honor the html options passed in' do
-    controller.stub!(:flash).and_return({:error => 'There was an error'})
-    view.flash_messages(:class => 'my-class').should == "<p class=\"my-class\" id=\"flash-error-message\">There was an error</p>"
+    @controller.stub!(:flash).and_return({:error => 'There was an error'})
+    @view.flash_messages(:class => 'my-class').should == "<div class=\"my-class\" id=\"flash-error\">There was an error</div>"
   end
 
 end
